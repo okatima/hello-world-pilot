7af4a6aa5e663df1781755b2d00c29d071d5c9c0
bladeecity <116978932+smokedope2k16@users.noreply.github.com>
2025-04-30 12:12:40 +0100

    fa

--- entities/GUI/canvas_layer.gd
+++ entities/GUI/canvas_layer.gd
@@ -2,11 +2,14 @@ extends CanvasLayer
 
 @onready var label: Label = $HBoxContainer/Label
 
+var number_of_coins: int = 0
+
 func _ready() -> void:
 	for coin in get_tree().get_nodes_in_group("coins"):
 		coin.coin_collected.connect(on_coin_collected)
 		
 
 func on_coin_collected() -> void:
-	print("You've collected a coin!")
-	label.text = "1"
+	print("You've collected a coin!, total number of coints: ", number_of_coins)
+	number_of_coins += 1
+	label.text = str(number_of_coins)
